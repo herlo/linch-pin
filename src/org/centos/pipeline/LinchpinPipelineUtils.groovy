1974215f42948948e042d5b0a20253c388d46cb3
Clint Savage <herlo@redhat.com>
2018-09-17 15:30:11 -0600

    Refactor out getFilesInChangeLogs() from methods

--- src/org/centos/pipeline/LinchpinPipelineUtils.groovy
+++ src/org/centos/pipeline/LinchpinPipelineUtils.groovy
@@ -10,6 +10,7 @@ def keysToList(keyValue) {
     return keys
 }
 
+
 def matchPath(path, paths){
     for (int i = 0; i < paths.size(); i++) {
         if (path ==~ ~/${paths[i]}/) {
@@ -19,35 +20,59 @@ def matchPath(path, paths){
     return false
 }
 
-def getTargetsToTest(targetsMap) {
-    def targets = [:]
+
+def getFilesInChangeLogs() {
+    def files = []
     def changeLogSets = currentBuild.changeSets
-    for (int i = 0; i < changeLogSets.size(); i++) {
+    sz = changeLogSets.size()
+    for (int i = 0; i < sz; i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
-            def files = new ArrayList(entry.affectedFiles)
-            for (int k = 0; k <files.size(); k++) {
-                def this_match = false
-                def file = files[k]
-                for (e in targetsMap) {
-                    if (matchPath(file.path, e.value)) {
-                        println "${e.key} matched ${file.path}"
-                        this_match = true
-                        targets[e.key] = 1
-                    }
-                }
-                if (!this_match) {
-                    // If we get here then we have a non-target specific change
-                    // and all targets should be tested.
-                    println "Non-target file matched, will test all targets"
-                    return keysToList(targetsMap)
-                }
+            files << new ArrayList(entry.affectedFiles)
+
+    return files
+}
+
+
+def getTargetsToTest(targetsMap) {
+    def targets = [:]
+    def files = getFilesInChangeLogs()
+    for (int k = 0; k <files.size(); k++) {
+        def this_match = false
+        def file = files[k]
+        for (e in targetsMap) {
+            if (matchPath(file.path, e.value)) {
+                println "${e.key} matched ${file.path}"
+                this_match = true
+                targets[e.key] = 1
             }
+        }
+        if (!this_match) {
+            // If we get here then we have a non-target specific change
+            // and all targets should be tested.
+            println "Non-target file matched, will test all targets"
+            return keysToList(targetsMap)
         }
     }
     return keysToList(targets)
 }
+
+
+// check the filePath against changed files
+// return file.path if there's a match, else false
+def getChangedFile(String filePath) {
+
+    def files = getFilesInChangeLogs()
+    for (int k = 0; k <files.size(); k++) {
+        def file = files[k]
+        if (file.path == filePath) {
+            return file.path
+        }
+    }
+    return false
+}
+
 
 def getProvidersToTest(providersMap) {
     def providers = [:]
@@ -78,6 +103,7 @@ def getProvidersToTest(providersMap) {
     }
     return keysToList(providers)
 }
+
 
 def sendPRComment(ghprbGhRepository, ghprbPullId, msg) {
     if (msg == null) {
